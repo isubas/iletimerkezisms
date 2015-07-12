38b074e0f794a19296cddc2d5e7913a9d341d6c5
İrfan SUBAŞ <irfan.subas@uzem.omu.edu.tr>
2015-07-12 16:04:52 +0300

    .

--- lib/iletimerkezisms/report.rb
+++ lib/iletimerkezisms/report.rb
@@ -4,49 +4,49 @@ require "iletimerkezisms/request"
 require 'nokogiri'
 
 module IletimerkeziSMS
-	class REPORT
-		def initialize(username, password)
-			@username = username
-			@password = password
-		end
+  class REPORT
+    def initialize(username, password)
+      @username = username
+      @password = password
+    end
 
-		def report(argv)
-			argv = {
-				page: 1,
-				rowCount: 1000
-				}.merge(argv)
+    def report(argv)
+      argv = {
+        page: 1,
+        rowCount: 1000
+        }.merge(argv)
 
-			path = "get-report"
+      path = "get-report"
 
-			xml_build = Nokogiri::XML::Builder.new do |xml|
-				xml.request {
-					xml.authentication {
-						xml.username @username
-						xml.password @password
-					}
-					xml.order{
-						xml.id argv[:id]
-						xml.page argv[:page]
-						xml.rowCount argv[:rowCount]
-					}
-				}
-			end
-			r = REQUEST.new(path,xml_build.to_xml)
-			return r.request
-		end
+      xml_build = Nokogiri::XML::Builder.new do |xml|
+        xml.request {
+          xml.authentication {
+            xml.username @username
+            xml.password @password
+          }
+          xml.order{
+            xml.id argv[:id]
+            xml.page argv[:page]
+            xml.rowCount argv[:rowCount]
+          }
+        }
+      end
+      r = REQUEST.new(path,xml_build.to_xml)
+      return r.request
+    end
 
-		def balance
-			path = "get-balance"
-			xml_build = Nokogiri::XML::Builder.new do |xml|
-				xml.request {
-					xml.authentication {
-						xml.username @username
-						xml.password @password
-					}
-				}
-			end
-			r = REQUEST.new(path,xml_build.to_xml)
-			return r.request
-		end
-	end
+    def balance
+      path = "get-balance"
+      xml_build = Nokogiri::XML::Builder.new do |xml|
+        xml.request {
+          xml.authentication {
+            xml.username @username
+            xml.password @password
+          }
+        }
+      end
+      r = REQUEST.new(path,xml_build.to_xml)
+      return r.request
+    end
+  end
 end
--- lib/iletimerkezisms/request.rb
+++ lib/iletimerkezisms/request.rb
@@ -4,20 +4,20 @@ require 'rest-client'
 require 'crack/xml'
 
 module IletimerkeziSMS
-	class REQUEST
-		def initialize(path, params)
-			@params = params
-			@api_base_url = "http://api.iletimerkezi.com/v1/#{path}"
-		end
+  class REQUEST
+    def initialize(path, params)
+      @params = params
+      @api_base_url = "http://api.iletimerkezi.com/v1/#{path}"
+    end
 
-		def request
-			begin
-				resource = RestClient::Resource.new(@api_base_url, timeout: 30, open_timeout: 30)
-				response = resource.post @params, content_type: :xml, accept: :xml
-			rescue RestClient::ExceptionWithResponse => err
-				response = err.response
-			end
-			return Crack::XML.parse(response.body)["response"]
-		end
-	end
+    def request
+      begin
+        resource = RestClient::Resource.new(@api_base_url, timeout: 30, open_timeout: 30)
+        response = resource.post @params, content_type: :xml, accept: :xml
+      rescue RestClient::ExceptionWithResponse => err
+        response = err.response
+      end
+      return Crack::XML.parse(response.body)["response"]
+    end
+  end
 end
